acce067bcc65d2485f28593651f41d475048b081
Vikrant Gawde <vikrant.gawde10@gmail.com>
2018-03-27 13:40:44 -0500

    Good Change

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.home,name='home'),
     path('blog/', include('blog.urls')),
     path('pics/', include('pics.urls')),
-
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
